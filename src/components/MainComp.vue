7bd46da06c581cf22dc9b378782ae73a3039e9b0
marcodldev <marcodelisi2@gmail.com>
2022-12-01 17:37:49 +0100

    emit

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,12 +1,19 @@
 <template>
     <div>
         maincomptest
+        <SearchComp />
+       <!-- // <button @click="$emit(emitSearch, lorem)"></button> -->
     </div>
 </template>
 
 <script>
+import SearchComp from "./SearchComp.vue";
+
 export default {
   name: "MainComp",
+  components: {
+    SearchComp,
+  },
 };
 </script>
 
--- src/components/SearchComp.vue
+++ src/components/SearchComp.vue
@@ -1,13 +1,41 @@
 <template>
   <div>
-    testsearch
+    <select name="" id="">
+        <option v-for="(elem,index) in dischi" :key="index" value="index"
+        >{{elem.genre}}</option>
+    </select>
   </div>
 </template>
 
 <script>
+import axios from "axios"
+
 export default {
   name: "SearchComp",
-};
+data() {
+    return {
+    dischi : '',
+    }
+},
+mounted () {
+    this.recuperaDischi ()
+    
+},
+methods : {
+    recuperaDischi () {
+        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+        .then((response) => {
+            this.dischi = response.data.response
+            
+        })
+    }
+
+
+},
+computed : {
+
+},
+}
 </script>
 
 <style lang="scss" scoped>
